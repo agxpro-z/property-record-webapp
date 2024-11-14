e414481cb0bf80bf0d599d7e1cac1f96fe39f537
agxpro <herrscherofdarkness@gmail.com>
2024-11-15 04:42:36 +0530

    Init: Create initial Next.js app.
    Signed-off-by: agxpro <herrscherofdarkness@gmail.com>

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Property Record App</h1>
+      <p>Hyperledger Fabric Property Record App</p>
+    </div>
+  );
+}
