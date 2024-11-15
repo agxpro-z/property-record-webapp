97894342c7db1248cd8050e5c062e81152896186
agxpro <herrscherofdarkness@gmail.com>
2024-11-15 07:26:21 +0530

    App: Add register page.
    Signed-off-by: agxpro <herrscherofdarkness@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import flattenColorPalette from "tailwindcss";
 
 export default {
   content: [
@@ -8,11 +9,30 @@ export default {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
+
+interface AddVariablesForColorsParams {
+  addBase: (styles: Record<string, Record<string, string>>) => void;
+  theme: (key: string) => string;
+}
+
+function addVariablesForColors({ addBase, theme }: AddVariablesForColorsParams) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
