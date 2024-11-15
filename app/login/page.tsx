21eb5f258f7cd6dd89b90580c3bfd2494f1fadd9
agxpro <herrscherofdarkness@gmail.com>
2024-11-15 21:44:47 +0530

    App: Add pages to login and register users.
    Also add button to main page to logout user.
    
    Signed-off-by: agxpro <herrscherofdarkness@gmail.com>

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,28 +8,22 @@ import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 
 export default function Register() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    await fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/users/register", {
+    await fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/users/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       credentials: "include",
       body: JSON.stringify({
-        firstName,
-        lastName,
         email,
         password,
-        confirmPassword,
       }),
     }).then((res) => {
       if (res.ok) {
@@ -44,35 +38,13 @@ export default function Register() {
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black border-solid border-[1px] border-zinc-800">
         <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-          Welcome to Property Record
+          Welcome back
         </h2>
         <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-          Sign up to keep track of your property records.
+          Sign in to see your property records.
         </p>
 
         <form className="my-8" onSubmit={handleSubmit}>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="firstname">First name</Label>
-              <Input
-                id="firstname"
-                placeholder="Shadow"
-                type="text"
-                onChange={e => setFirstName(e.target.value)}
-                value={firstName}
-              />
-            </LabelInputContainer>
-            <LabelInputContainer>
-              <Label htmlFor="lastname">Last name</Label>
-              <Input
-                id="lastname"
-                placeholder="Pro"
-                type="text"
-                onChange={e => setLastName(e.target.value)}
-                value={lastName}
-              />
-            </LabelInputContainer>
-          </div>
           <LabelInputContainer className="mb-4">
             <Label htmlFor="email">Email Address</Label>
             <Input
@@ -93,22 +65,12 @@ export default function Register() {
               value={password}
             />
           </LabelInputContainer>
-          <LabelInputContainer className="mb-8">
-            <Label htmlFor="confirmpassword">Confirm password</Label>
-            <Input
-              id="confirmpassword"
-              placeholder="••••••••"
-              type="password"
-              onChange={e => setConfirmPassword(e.target.value)}
-              value={confirmPassword}
-            />
-          </LabelInputContainer>
 
           <button
             className="bg-gradient-to-br relative group/btn border-zinc-800 border-[1.5px] from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             type="submit"
           >
-            Sign up &rarr;
+            Sign in &rarr;
             <BottomGradient />
           </button>
 
@@ -128,7 +90,6 @@ export default function Register() {
           </div>
         </form>
       </div>
-
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,49 @@
+'use client';
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  const logoutHandler = async () => {
+    await fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/users/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }).then((res) => {
+      if (res.ok) {
+        console.log("Logged out");
+        router.push("/login");
+      } else {
+        console.error("Failed to logout");
+      }
+    });
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold">Property Record App</h1>
       <p>Hyperledger Fabric Property Record App</p>
+
+      <div className="max-w-xs w-full mt-4">
+        <button
+          className="bg-gradient-to-br relative group/btn border-zinc-800 border-[1.5px] from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          onClick={logoutHandler}
+        >
+          Logout &rarr;
+          <BottomGradient />
+        </button>
+      </div>
     </div>
   );
 }
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
